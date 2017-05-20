6913d0903a6846a5203c6c96315d579da4af26c9
Justin Santa Barbara <justin@fathomdb.com>
2017-05-19 22:32:22 -0400

    Add material-ui & react-router

--- webapp/portal/src/App.js
+++ webapp/portal/src/App.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import UserComponent from "./components/UserComponent";
+import {MuiThemeProvider} from "material-ui/styles";
+import {BrowserRouter, Route} from "react-router-dom";
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <BrowserRouter>
+        <div>
+          <MuiThemeProvider>
+            <div>
+              <Route exact path="/" render={ () => <UserComponent /> }/>
+            </div>
+          </MuiThemeProvider>
+
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </BrowserRouter>
     );
   }
 }
