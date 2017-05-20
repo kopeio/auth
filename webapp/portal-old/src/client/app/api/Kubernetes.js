41de3541b22c42b574a39b9060a53a7a8adc3605
Justin Santa Barbara <justin@fathomdb.com>
2017-05-20 00:59:00 -0400

    Remove old webapp

--- webapp/portal-old/src/client/app/api/Kubernetes.js
+++
@@ -1,7 +0,0 @@
-class Kubernetes {
-  static url(group, version) {
-      return "https://userapimock.useast1.k8s.justinsb.com/apis/" + group + "/" + version + "/";
-  }
-};
-
-export default Kubernetes;
